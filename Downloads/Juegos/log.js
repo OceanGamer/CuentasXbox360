5d8437e41c36afdc0ad760b430c7f7fb933054e1
OceanGamer <100978092+OceanGamer@users.noreply.github.com>
2022-03-13 04:01:41 +0100

    Update log.js

--- Downloads/Juegos/log.js
+++ Downloads/Juegos/log.js
@@ -1,4 +1,4 @@
 function login(){
-    let usuario = document.getElementById("user")
-    let password = document.getElementById("password")
+    let usuario = document.getElementById("user").value
+    let password = document.getElementById("password").value
 }
