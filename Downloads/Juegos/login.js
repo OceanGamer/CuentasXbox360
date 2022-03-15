27081eb7856e7ddf10fdfdec24a7353530ee4421
OceanGamer <100978092+OceanGamer@users.noreply.github.com>
2022-03-15 02:24:58 +0100

    Add files via upload

---
+++ Downloads/Juegos/login.js
@@ -0,0 +1,23 @@
+const formlog = document.getElementById("login-form");
+const BtnLog = document.getElementById("login-form-submit");
+const ErrorMessage = document.getElementById("login-error-msg");
+const weblink = document.getElementById("final-web-link");
+
+BtnLog.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = formlog.username.value;
+    const password = formlog.password.value;
+    const link = formlog.link.value;
+
+    if (username === "OceanGamer" && password === "Mods1008") {
+        alert("Logueo Exitoso!");
+        window.location.href = link;
+    }
+     else if (username === "MoisesGamers19" && password === "TheKing19") {
+        alert("Logueo Exitoso!");
+        location.reload();
+    } else {
+        alert("Usuario o Clave Incorrecto!");
+        location.reload();
+    }
+})
